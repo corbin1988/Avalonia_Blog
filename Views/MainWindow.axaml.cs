399b13b924481af05f7390d744990f12f826476b
KyleHurst <kyle.c.hurst@gmail.com>
2024-06-16 15:49:47 -0400

    Add developer tools

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 
 namespace Avalonia_Blog.Views;
@@ -7,5 +8,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        // This project will never be in prod,
+        // so we can safely attach the dev tools.
+        this.AttachDevTools();
     }
 }
